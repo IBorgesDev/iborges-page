829a563fdb1355a7094d7759e1510dbbd1b21b4f
Icaro Borges <contatosiquis@gmail.com>
2023-09-29 22:12:11 -0300

    teste final

--- iborgespage/src/App.js
+++ iborgespage/src/App.js
@@ -1,39 +1,30 @@
-
 import './App.css';
-
 import logo from "./Assets/ibwhite.svg"
 
 function App() {
   return (
     <main>
       <header className='header-container'>
-        
-        <img className = "logo" src={logo}></img>
-        <nav className ='menu-options'>
-        
-        <a>Home</a>
-        <a>Projects</a>
-        <a>About Me</a>
+        {/* Provide meaningful alt text for the image */}
+        <img className="logo" src={logo} alt="IBorgesDev Logo" />
+        <nav className='menu-options'>
+          <a href="#home">Home</a>
+          <a href="#projects">Projects</a>
+          <a href="#about">About Me</a>
         </nav>
-        <button class="glow-on-hover" type="button">Contact me!</button>
-        
+        <button className="glow-on-hover" type="button">Contact me!</button>
       </header>
-
-
-      <div class="div1">
-      <div class="table-display">
-        <div class="cell-display div2"></div>
-        <div class="cell-display">
-          <div class="div3"></div>
-          <div class="div4"></div>
+      
+      <div className="div1">
+        <div className="table-display">
+          <div className="cell-display div2"></div>
+          <div className="cell-display">
+            <div className="div3"></div>
+            <div className="div4"></div>
+          </div>
         </div>
       </div>
-    </div>
     </main>
-    
-    
-
-
   );
 }
 
