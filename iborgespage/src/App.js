3a28fde33165d8ae7811ba05dfb6e9707202d2c6
Icaro Borges <contatosiquis@gmail.com>
2023-09-29 21:52:13 -0300

    Update
    blabla

--- iborgespage/src/App.js
+++ iborgespage/src/App.js
@@ -7,15 +7,28 @@ function App() {
   return (
     <main>
       <header className='header-container'>
-        <img src={logo}></img>
+        
+        <img className = "logo" src={logo}></img>
         <nav className ='menu-options'>
         
         <a>Home</a>
         <a>Projects</a>
         <a>About Me</a>
         </nav>
-        <button>Contact Me</button>
+        <button class="glow-on-hover" type="button">Contact me!</button>
+        
       </header>
+
+
+      <div class="div1">
+      <div class="table-display">
+        <div class="cell-display div2"></div>
+        <div class="cell-display">
+          <div class="div3"></div>
+          <div class="div4"></div>
+        </div>
+      </div>
+    </div>
     </main>
     
     
