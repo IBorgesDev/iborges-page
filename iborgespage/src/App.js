5ce5767dd8fb57402a4b46fa42cb72cf4a3b5495
Icaro Borges <contatosiquis@gmail.com>
2023-09-30 12:33:14 -0300

    update

--- iborgespage/src/App.js
+++ iborgespage/src/App.js
@@ -8,12 +8,19 @@ function App() {
         {/* Provide meaningful alt text for the image */}
         <img className="logo" src={logo} alt="IBorgesDev Logo" />
         <nav className='menu-options'>
-          <a href="#home">Home</a>
-          <a href="#projects">Projects</a>
-          <a href="#about">About Me</a>
+          <a href="Home">Home</a>
+          <a href="Projects">Projects</a>
+          <a href="About">About Me</a>
         </nav>
         <button className="glow-on-hover" type="button">Contact me!</button>
+        
       </header>
+      <div className='content-container'>
+            <h1>Who am i?</h1>
+            <h2>My name is Ícaro Borges</h2>
+            <h3>Software Engineer</h3>
+            <h1>Tools and Languages</h1>
+        </div>
     </main>
   );
 }
